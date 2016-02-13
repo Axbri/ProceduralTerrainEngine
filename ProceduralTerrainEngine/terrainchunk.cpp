c02265cfb96898f9bb81b5ba8d1c27c80cb113ac
Axbri <axel.brinkeby@gmail.com>
2016-02-13 22:55:30 +0100

    Fixat så texturkoordinaterna är samma för olika terräng LOD nivåer

--- ProceduralTerrainEngine/terrainchunk.cpp
+++ ProceduralTerrainEngine/terrainchunk.cpp
@@ -33,25 +33,20 @@ TerrainChunk::TerrainChunk(Loader loader, double x, double z)
 				double height = TerrainHeightGenerator::getHeight(xPos + localVertexPosX, zPos + localVertexPosZ);
 				vertexHeights[x][z] = height;
 				positions[positionIndex++] = xPos + localVertexPosX;
-				
-				
+								
 				if (x == 0 || z == 0 || x == numberOfVertices[i]-1 || z == numberOfVertices[i]-1)
 				{
 					if (i == 1)
 						height -= 0.1;
 					else if (i == 2)
 						height -= 0.5;
-				}
-
-				
+				}								
 
 				positions[positionIndex++] = (float)height;
-
-
 				positions[positionIndex++] = zPos + localVertexPosZ;
 
-				float texCoordU = (float)(x) / (float)numberOfVertices[i];
-				float texCoordV = (float)(z) / (float)numberOfVertices[i];
+				float texCoordU = (float)(x) / (float)(numberOfVertices[i] - 1);
+				float texCoordV = (float)(z) / (float)(numberOfVertices[i] - 1);
 				textureCoords[textureCoordIndex++] = texCoordU;
 				textureCoords[textureCoordIndex++] = texCoordV;
 
